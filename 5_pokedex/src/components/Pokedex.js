c6a76fea24da967b8ae5021131673941be7886b6
Lalo Aguilar <tallitosan@gmail.com>
2021-05-05 01:00:12 -0500

    done pokedex project

---
+++ 5_pokedex/src/components/Pokedex.js
@@ -0,0 +1,34 @@
+import { Component } from 'react'
+import Pokecard from './Pokecard'
+import './Pokedex.css'
+
+class Pokedex extends Component {
+  render() {
+    let title
+
+    if (this.props.isWinner) {
+      title = <h1 class='Pokedex-winner'>Winning Hand</h1>
+    } else {
+      title = <h1 class='Pokedex-loser'>Losing Hand</h1>
+    }
+
+    return (
+      <div className='Pokedex'>
+        {title}
+        <h4>Total experience: {this.props.exp}</h4>
+        <div className='Pokedex-cards'>
+          {this.props.pokemon.map((p) => (
+            <Pokecard
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Pokedex
