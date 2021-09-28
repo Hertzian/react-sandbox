06c99b16b09a1b06eb31d79ecfd511a1a3024f78
Hertzian <tallitosan@gmail.com>
2021-09-28 01:50:28 -0500

    18_deck-of-card done, todo styling

---
+++ 18_deck-of-cards/src/App.js
@@ -0,0 +1,13 @@
+import Deck from './Deck'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>App</h1>
+      <Deck />
+    </div>
+  )
+}
+
+export default App
---
+++ 18_deck-of-cards/src/Card.js
@@ -0,0 +1,9 @@
+import { Component } from 'react'
+
+class Card extends Component {
+  render() {
+    return <img className='Card' src={this.props.image} alt={this.props.name} />
+  }
+}
+
+export default Card
