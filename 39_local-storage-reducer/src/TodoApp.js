239c3ce7585f0079db9fe293ba3acecb560de941
Hertzian <tallitosan@gmail.com>
2021-10-30 01:47:04 -0500

    done added useLocalStorageReducer, *complex topic*

---
+++ 39_local-storage-reducer/src/TodoApp.js
@@ -0,0 +1,40 @@
+import TodoList from './TodoList'
+import TodoForm from './TodoForm'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/Toolbar'
+import Grid from '@material-ui/core/Grid'
+import { TodosProvider } from './context/todos.context.js'
+import MentisApp from './MentisApp'
+
+function TodoApp() {
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        marging: 0,
+        height: '100vh',
+        backgroundColor: '#fafafa'
+      }}
+      elevation={0}
+    >
+      <AppBar color='primary' position='static' style={{ height: '64px' }}>
+        <ToolBar>
+          <Typography color='inherit'>todos with hoooks</Typography>
+        </ToolBar>
+      </AppBar>
+      <Grid container justify='center' style={{ marginTop: '1rem' }}>
+        <Grid item xs={11} md={8} lg={4}>
+          <TodosProvider>
+            <TodoForm />
+            <TodoList />
+          </TodosProvider>
+        </Grid>
+      </Grid>
+      <MentisApp />
+    </Paper>
+  )
+}
+
+export default TodoApp
---
+++ 39_local-storage-reducer/src/TodoForm.js
@@ -0,0 +1,32 @@
+import { useContext } from 'react'
+import useInputState from './hooks/useInputState'
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper'
+import { DispatchContext } from './context/todos.context'
+
+function TodoForm() {
+  const [value, handleChange, reset] = useInputState('')
+  const dispatch = useContext(DispatchContext)
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch({ type: 'ADD', task: value })
+    reset()
+  }
+
+  return (
+    <Paper style={{ margin: '1rem 0', padding: '0 1rem' }}>
+      <form onSubmit={submitHandler}>
+        <TextField
+          value={value}
+          onChange={handleChange}
+          margin='normal'
+          label='Add new todo'
+          fullWidth
+        />
+      </form>
+    </Paper>
+  )
+}
+
+export default TodoForm
